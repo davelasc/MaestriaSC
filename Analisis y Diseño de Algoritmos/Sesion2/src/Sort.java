44e8ff2a554925b785b6059ffc70bb2f9a9fdde6
David Velasco <velascos.davida@gmail.com>
2017-08-28 21:43:33 -0500

    subiendo avances de la tarea. falta hacer los promedios

--- Analisis y Diseño de Algoritmos/Sesion2/src/Sort.java
+++ Analisis y Diseño de Algoritmos/Sesion2/src/Sort.java
@@ -64,16 +64,47 @@ public class Sort {
 		return counter;
 	}
 	
+	public static int shellSort(int[] array) {
+		int inner, outer, temp, comparaciones = 0, saltos = 0;
+		movimientos = 0;
+		
+		while(saltos <= array.length / 3) {
+			saltos = saltos * 3 + 1;
+		}
+		 
+		while(saltos > 0) {
+			
+			for(outer = saltos; outer < array.length; outer++) {
+				temp = array[outer];
+				inner = outer;
+				while(inner > saltos - 1) {
+					comparaciones++;
+					if(array[inner - saltos] >= temp) {
+						array[inner] = array[inner - saltos];
+						inner -= saltos;
+						movimientos++;
+					} else {
+						break;
+					}
+				}
+				array[inner] = temp;
+			}
+			saltos = (saltos - 1) / 3;
+		}
+		
+		return comparaciones;
+	}
+	
 	public static void main(String[] args) {
-		/*//int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
+		/*/int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
 		int[] array = new int[] {9,8,7,6,5,4,3,2,1,0};
 		Utils.printArray(array);
-		System.out.println("Comparaciones hechas: " + bubbleSort(array));
+		System.out.println("Comparaciones hechas: " + shellSort(array));
 		System.out.println("Movimientos hechos: " + movimientos);/**/
 		int[] array;
 		int min = -200, max = 200;
 		System.out.println("Elementos en el arreglo;Comparaciones;Movimientos");
-		for(int i = 1; i <= 200; i++) {
+		for(int i = 1000; i <= 60000; i+=1000) {
 			array = Utils.createIntArray(i, min, max);
 			System.out.println(i + ";" + bubbleSort(array) + ";" + movimientos);
 		}/**/
